86403ad5fe6757ed0ce2788fadffadf9e314e987
Zachary Huff <zach.huff.386@gmail.com>
2018-02-02 00:26:08 -0500

    Remove logger

--- main.go
+++ main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
-	"github.com/pritunl/terraform-provider-mongodbatlas/logger"
 	"github.com/pritunl/terraform-provider-mongodbatlas/provider"
 )
 
 func main() {
-	logger.Init()
-
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
 			return provider.Provider()
